10ed9bf15bf5a9e9ec9b6b85f9ee6a73e6b69ce8
swaroop832 <sc6f6@mail.umkc.edu>
2018-04-13 16:24:11 -0500

    initial code

---
+++ SourceCode/app.js
@@ -0,0 +1,10 @@
+let app = angular.module("app",[]);
+app.controller('MainCtrl',function ($scope,$http) {
+
+
+    $http.get("sql.php").then(function (response) {
+      $scope.data = response.data.records;
+      console.log("hi");
+      console.log($scope.data);
+    });
+});
