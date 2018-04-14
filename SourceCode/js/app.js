eeb8f3dbd675930e0d3fcacf50f99a0c85f50199
NikhilaChinthakindhi <nikhila.reddy525@gmail.com>
2018-04-13 19:32:32 -0500

    code

---
+++ SourceCode/js/app.js
@@ -0,0 +1,8 @@
+let app = angular.module("app",[]);
+app.controller('MainCtrl',function ($scope,$http) {
+    $http.get("../php/sql.php").then(function (response) {
+      $scope.data = response.data.records;
+      console.log("hi");
+      console.log($scope.data);
+    });
+});
