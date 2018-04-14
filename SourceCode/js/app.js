a50e6710d9fa1e757031de2be3431acfbffd19e4
swaroop832 <sc6f6@mail.umkc.edu>
2018-04-13 19:35:28 -0500

    minor changes

--- SourceCode/js/app.js
+++ SourceCode/js/app.js
@@ -1,5 +1,6 @@
 let app = angular.module("app",[]);
 app.controller('MainCtrl',function ($scope,$http) {
+
     $http.get("../php/sql.php").then(function (response) {
       $scope.data = response.data.records;
       console.log("hi");
